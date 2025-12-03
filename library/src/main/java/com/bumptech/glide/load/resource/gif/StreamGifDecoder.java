8ac5b23e55b90b8aab727b61c551deed0622631e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 12:57:14 +0000

    Revert "fix: 77 - UNBOX_NULLABLE - return !options.get(GifOptions.DISABLE_ANIMATION) - unboxing of a @Nullable value - options.get(GifOptions.DISABLE_ANIMATION)"
    This reverts commit d67467cf56994f1fe94ff0293e686b643fb7278b.

--- library/src/main/java/com/bumptech/glide/load/resource/gif/StreamGifDecoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/gif/StreamGifDecoder.java
@@ -39,11 +39,7 @@ public class StreamGifDecoder implements ResourceDecoder<InputStream, GifDrawabl
 
   @Override
   public boolean handles(@NonNull InputStream source, @NonNull Options options) throws IOException {
-    Boolean disableAnimation = options.get(GifOptions.DISABLE_ANIMATION);
-    if (disableAnimation == null) {
-      disableAnimation = Boolean.FALSE;
-    }
-    return !disableAnimation
+    return !options.get(GifOptions.DISABLE_ANIMATION)
         && ImageHeaderParserUtils.getType(parsers, source, byteArrayPool) == ImageType.GIF;
   }
 
