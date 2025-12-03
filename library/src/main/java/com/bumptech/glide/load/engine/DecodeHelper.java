0c734fecaecc791695c548cb89b2c03802957f83
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 11:12:39 +0000

    Revert "fix: 17 - ASSIGN_FIELD_NULLABLE - transcodeClass = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 6b48a41b87283033f21a0a473e10d4ef034460e9.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -36,7 +36,7 @@ final class DecodeHelper<Transcode> {
   private DecodeJob.DiskCacheProvider diskCacheProvider;
   private Options options;
   @Nullable private Map<Class<?>, Transformation<?>> transformations;
-  @Nullable private Class<Transcode> transcodeClass;
+  private Class<Transcode> transcodeClass;
   private boolean isLoadDataSet;
   private boolean isCacheKeysSet;
   @Nullable private Key signature;
