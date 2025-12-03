7d30d536ebe74bbf4f346024e6fe80acba37b870
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 12:02:12 +0000

    Revert "fix: 46 - DEREFERENCE_NULLABLE - sourceKey, data, loadData.fetcher, DataSource.RESOURCE_DISK_CACHE, currentKey); - dereferenced expression loadData is @Nullable"
    This reverts commit de42f21d34b4fef4e99be5a9a65c8cd584f56a8c.

--- library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
@@ -129,12 +129,8 @@ class ResourceCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCa
 
   @Override
   public void onDataReady(@Nullable Object data) {
-    LoadData<?> local = loadData;
-    if (local == null) {
-      return;
-    }
     cb.onDataFetcherReady(
-        sourceKey, data, local.fetcher, DataSource.RESOURCE_DISK_CACHE, currentKey);
+        sourceKey, data, loadData.fetcher, DataSource.RESOURCE_DISK_CACHE, currentKey);
   }
 
   @Override
