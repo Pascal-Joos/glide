da0d8b15900565a63f04e6b329979c33fc482bdc
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 12:16:19 +0000

    Revert "fix: 52 - DEREFERENCE_NULLABLE - last.key.offer(); - dereferenced expression last.key is @Nullable"
    This reverts commit 4e4f19106134ac801067c474ebc0a3d84fd8edb4.

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/GroupedLinkedMap.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/GroupedLinkedMap.java
@@ -68,9 +68,7 @@ class GroupedLinkedMap<K extends Poolable, V> {
         // sizes.
         removeEntry(last);
         keyToEntry.remove(last.key);
-        if (last.key != null) {
-          last.key.offer();
-        }
+        last.key.offer();
       }
 
       last = last.prev;
