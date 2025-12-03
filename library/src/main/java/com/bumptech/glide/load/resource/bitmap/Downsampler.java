edf51b42154d3a9da09c26a7653dd7ddd50f1358
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 12:47:31 +0000

    Revert "fix: 71 - UNBOX_NULLABLE - boolean fixBitmapToRequestedDimensions = options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS); - unboxing of a @Nullable value - options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS)"
    This reverts commit 9ff3a2a022ee166f1a0e0aa040df5379b4591bd1.

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
@@ -283,10 +283,7 @@ public final class Downsampler {
     DecodeFormat decodeFormat = options.get(DECODE_FORMAT);
     PreferredColorSpace preferredColorSpace = options.get(PREFERRED_COLOR_SPACE);
     DownsampleStrategy downsampleStrategy = options.get(DownsampleStrategy.OPTION);
-    Boolean fixBitmapToRequestedDimensionsOption =
-        options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS);
-    boolean fixBitmapToRequestedDimensions =
-        fixBitmapToRequestedDimensionsOption != null && fixBitmapToRequestedDimensionsOption;
+    boolean fixBitmapToRequestedDimensions = options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS);
     boolean isHardwareConfigAllowed =
         options.get(ALLOW_HARDWARE_CONFIG) != null && options.get(ALLOW_HARDWARE_CONFIG);
 
