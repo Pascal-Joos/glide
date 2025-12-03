a2550a3142656364847f19b986a3954e1f7ec044
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 11:52:57 +0000

    Revert "fix: 40 - ASSIGN_FIELD_NULLABLE - decodeJob = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit c73d344ce9f8cb740bacf02703f9406dd57a01eb.

--- library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
@@ -70,7 +70,7 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
   @Synthetic
   EngineResource<?> engineResource;
 
-  @Nullable private DecodeJob<R> decodeJob;
+  private DecodeJob<R> decodeJob;
 
   // Checked primarily on the main thread, but also on other threads in reschedule.
   private volatile boolean isCancelled;
