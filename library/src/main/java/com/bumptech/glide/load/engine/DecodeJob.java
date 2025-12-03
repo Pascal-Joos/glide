56d407e03dbccb32c00fe2b18a3ecdb7832a7dbd
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 11:41:24 +0000

    fix: 34 - DEREFERENCE_NULLABLE - return path.load( - dereferenced expression path is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -546,6 +546,9 @@ class DecodeJob<R>
   private <Data, ResourceType> Resource<R> runLoadPath(
       Data data, @Nullable DataSource dataSource, @Nullable LoadPath<Data, ResourceType, R> path)
       throws GlideException {
+    if (path == null) {
+      throw new GlideException("Expected to receive a non-null LoadPath");
+    }
     Options options = getOptionsWithHardwareConfig(dataSource);
     DataRewinder<Data> rewinder = glideContext.getRegistry().getRewinder(data);
     try {
