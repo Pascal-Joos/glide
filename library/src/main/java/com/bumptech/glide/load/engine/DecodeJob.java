4cffd21c4c088409a7c900f32262d9cc3bfdcb42
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 11:33:48 +0000

    fix: 30 - ASSIGN_FIELD_NULLABLE - options = null; - assigning @Nullable expression to @NonNull field

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -180,7 +180,6 @@ class DecodeJob<R>
     isCallbackNotified = false;
     glideContext = null;
     signature = null;
-    options = null;
     priority = null;
     loadKey = null;
     callback = null;
