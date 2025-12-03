1daee8b3e2120e0e01d98974d120dfe6957e44df
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 11:02:03 +0000

    fix: 10 - DEREFERENCE_NULLABLE - cb.onDataFetcherReady(sourceKey, data, loadData.fetcher, DataSource.DATA_DISK_CACHE, sourceKey); - dereferenced expression loadData is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
@@ -100,7 +100,10 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
 
   @Override
   public void onDataReady(@Nullable Object data) {
-    cb.onDataFetcherReady(sourceKey, data, loadData.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
+    LoadData<?> local = loadData;
+    if (local != null) {
+      cb.onDataFetcherReady(sourceKey, data, local.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
+    }
   }
 
   @Override
