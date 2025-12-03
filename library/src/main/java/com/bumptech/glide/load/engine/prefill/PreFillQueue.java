4e911f02d70f396b04648dd694593c99d6c275fb
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 12:31:16 +0000

    Revert "fix: 61 - UNBOX_NULLABLE - if (countForResult == 1) { - unboxing of a @Nullable value - countForResult"
    This reverts commit fb9ed38cbeb04beb2f75ccd472ec3a92bfa3ff65.

--- library/src/main/java/com/bumptech/glide/load/engine/prefill/PreFillQueue.java
+++ library/src/main/java/com/bumptech/glide/load/engine/prefill/PreFillQueue.java
@@ -25,12 +25,11 @@ final class PreFillQueue {
     PreFillType result = keyList.get(keyIndex);
 
     Integer countForResult = bitmapsPerType.get(result);
-    int count = countForResult == null ? 0 : countForResult;
-    if (count <= 1) {
+    if (countForResult == 1) {
       bitmapsPerType.remove(result);
       keyList.remove(keyIndex);
     } else {
-      bitmapsPerType.put(result, count - 1);
+      bitmapsPerType.put(result, countForResult - 1);
     }
     bitmapsRemaining--;
 
