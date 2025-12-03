4f24140ba844a8a788ce2b6606b7142bf5e307f3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 11:32:16 +0000

    Revert "fix: 29 - ASSIGN_FIELD_NULLABLE - signature = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit b6ac87ea1d54d7fbba67c1d394dc8adb3cd3f785.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -53,7 +53,7 @@ class DecodeJob<R>
   private final ReleaseManager releaseManager = new ReleaseManager();
 
   private GlideContext glideContext;
-  @Nullable private Key signature;
+  private Key signature;
   @Nullable private Priority priority;
   @Nullable private EngineKey loadKey;
   private int width;
@@ -616,15 +616,14 @@ class DecodeJob<R>
       final Key key;
       switch (encodeStrategy) {
         case SOURCE:
-          key =
-              new DataCacheKey(currentSourceKey, signature == null ? currentSourceKey : signature);
+          key = new DataCacheKey(currentSourceKey, signature);
           break;
         case TRANSFORMED:
           key =
               new ResourceCacheKey(
                   decodeHelper.getArrayPool(),
                   currentSourceKey,
-                  signature == null ? currentSourceKey : signature,
+                  signature,
                   width,
                   height,
                   appliedTransformation,
