4d2ee2a08d226fcd9792049522ffd752d424c275
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 12:45:56 +0000

    Revert "fix: 70 - UNBOX_NULLABLE - options.get(ALLOW_HARDWARE_CONFIG) != null && options.get(ALLOW_HARDWARE_CONFIG); - unboxing of a @Nullable value - options.get(ALLOW_HARDWARE_CONFIG)"
    This reverts commit 8dece0b02b26417f9888a9570797ab279d72e432.

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
@@ -284,8 +284,8 @@ public final class Downsampler {
     PreferredColorSpace preferredColorSpace = options.get(PREFERRED_COLOR_SPACE);
     DownsampleStrategy downsampleStrategy = options.get(DownsampleStrategy.OPTION);
     boolean fixBitmapToRequestedDimensions = options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS);
-    @Nullable Boolean allowHardwareConfig = options.get(ALLOW_HARDWARE_CONFIG);
-    boolean isHardwareConfigAllowed = Boolean.TRUE.equals(allowHardwareConfig);
+    boolean isHardwareConfigAllowed =
+        options.get(ALLOW_HARDWARE_CONFIG) != null && options.get(ALLOW_HARDWARE_CONFIG);
 
     try {
       Bitmap result =
