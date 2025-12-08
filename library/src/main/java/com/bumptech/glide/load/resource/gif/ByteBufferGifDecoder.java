2407015bf0dd16fa9b7fcee06bc2dad1860127c1
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 13:59:08 +0000

    fix: Type='UNBOX_NULLABLE', message='unboxing of a @Nullable value - options.get(GifOptions.DISABLE_ANIMATION)' /home/vscode/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/resource/gif/ByteBufferGifDecoder.java:81     return !options.get(GifOptions.DISABLE_ANIMATION)

--- library/src/main/java/com/bumptech/glide/load/resource/gif/ByteBufferGifDecoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/gif/ByteBufferGifDecoder.java
@@ -78,7 +78,7 @@ public class ByteBufferGifDecoder implements ResourceDecoder<ByteBuffer, GifDraw
 
   @Override
   public boolean handles(@NonNull ByteBuffer source, @NonNull Options options) throws IOException {
-    return !options.get(GifOptions.DISABLE_ANIMATION)
+    return Boolean.FALSE.equals(options.get(GifOptions.DISABLE_ANIMATION))
         && ImageHeaderParserUtils.getType(parsers, source) == ImageType.GIF;
   }
 
