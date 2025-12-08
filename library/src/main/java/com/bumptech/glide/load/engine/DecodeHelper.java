6fbc5402d87f0ed6c464bbe59cc9bd81d9f472f0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 12:38:41 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression transformations is @Nullable' /home/vscode/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java:163     Transformation<Z> result = (Transformation<Z>) transformations.get(resourceClass);

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -169,9 +169,21 @@ final class DecodeHelper<Transcode> {
 
   @SuppressWarnings("unchecked")
   <Z> Transformation<Z> getTransformation(Class<Z> resourceClass) {
-    Transformation<Z> result = (Transformation<Z>) transformations.get(resourceClass);
+    Map<Class<?>, Transformation<?>> localTransformations = transformations;
+    if (localTransformations == null || localTransformations.isEmpty()) {
+      if (isTransformationRequired) {
+        throw new IllegalArgumentException(
+            "Missing transformation for "
+                + resourceClass
+                + ". If you wish to"
+                + " ignore unknown resource types, use the optional transformation methods.");
+      }
+      return UnitTransformation.get();
+    }
+
+    Transformation<Z> result = (Transformation<Z>) localTransformations.get(resourceClass);
     if (result == null) {
-      for (Entry<Class<?>, Transformation<?>> entry : transformations.entrySet()) {
+      for (Entry<Class<?>, Transformation<?>> entry : localTransformations.entrySet()) {
         if (entry.getKey().isAssignableFrom(resourceClass)) {
           result = (Transformation<Z>) entry.getValue();
           break;
@@ -180,7 +192,7 @@ final class DecodeHelper<Transcode> {
     }
 
     if (result == null) {
-      if (transformations.isEmpty() && isTransformationRequired) {
+      if (isTransformationRequired) {
         throw new IllegalArgumentException(
             "Missing transformation for "
                 + resourceClass
