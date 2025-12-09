ed0627b7b95043b9a2fc722af09bb79e31b8bfbb
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 15:29:50 +0000

    Revert "fix: 15 - ASSIGN_FIELD_NULLABLE - model = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 4da8dcd408b044861bf61a93554ed480cb449eeb.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -16,7 +16,6 @@ import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoader.LoadData;
 import com.bumptech.glide.load.resource.UnitTransformation;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +29,7 @@ final class DecodeHelper<Transcode> {
   private final List<Key> cacheKeys = new ArrayList<>();
 
   private GlideContext glideContext;
-  @Nullable private Object model;
+  private Object model;
   private int width;
   private int height;
   private Class<?> resourceClass;
@@ -137,14 +136,13 @@ final class DecodeHelper<Transcode> {
   }
 
   Class<?> getModelClass() {
-    return Nullability.castToNonnull(model.getClass());
+    return model.getClass();
   }
 
   List<Class<?>> getRegisteredResourceClasses() {
     return glideContext
         .getRegistry()
-        .getRegisteredResourceClasses(
-            Nullability.castToNonnull(model), resourceClass, transcodeClass);
+        .getRegisteredResourceClasses(model.getClass(), resourceClass, transcodeClass);
   }
 
   boolean hasLoadPath(Class<?> dataClass) {
@@ -215,12 +213,11 @@ final class DecodeHelper<Transcode> {
     if (!isLoadDataSet) {
       isLoadDataSet = true;
       loadData.clear();
-      List<ModelLoader<Object, ?>> modelLoaders =
-          glideContext.getRegistry().getModelLoaders(Nullability.castToNonnull(model));
+      List<ModelLoader<Object, ?>> modelLoaders = glideContext.getRegistry().getModelLoaders(model);
+      //noinspection ForLoopReplaceableByForEach to improve perf
       for (int i = 0, size = modelLoaders.size(); i < size; i++) {
         ModelLoader<Object, ?> modelLoader = modelLoaders.get(i);
-        LoadData<?> current =
-            modelLoader.buildLoadData(Nullability.castToNonnull(model), width, height, options);
+        LoadData<?> current = modelLoader.buildLoadData(model, width, height, options);
         if (current != null) {
           loadData.add(current);
         }
