664f47fc5533dd371ac200bd4ac0e41e1ff4dc92
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 15:24:42 +0000

    Revert "fix: 12 - PASS_NULLABLE - modelLoader.buildLoadData( - passing @Nullable parameter 'cacheFile' where @NonNull is required"
    This reverts commit 440d60829fd46c0702abbdf7762b1b1fadfef665.

--- library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
@@ -7,7 +7,6 @@ import com.bumptech.glide.load.data.DataFetcher;
 import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoader.LoadData;
 import com.bumptech.glide.util.pool.GlideTrace;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.util.List;
 import javax.annotation.Nullable;
@@ -57,6 +56,8 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
         }
 
         Key sourceId = cacheKeys.get(sourceIdIndex);
+        // PMD.AvoidInstantiatingObjectsInLoops The loop iterates a limited number of times
+        // and the actions it performs are much more expensive than a single allocation.
         @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
         Key originalKey = new DataCacheKey(sourceId, helper.getSignature());
         cacheFile = helper.getDiskCache().get(originalKey);
@@ -73,10 +74,7 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
         ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
         loadData =
             modelLoader.buildLoadData(
-                Nullability.castToNonnull(cacheFile),
-                helper.getWidth(),
-                helper.getHeight(),
-                helper.getOptions());
+                cacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
         if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
           started = true;
           loadData.fetcher.loadData(helper.getPriority(), this);
