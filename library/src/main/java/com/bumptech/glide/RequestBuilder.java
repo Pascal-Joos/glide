b6f87829198e730f01f73cf4f7242e78f0b77b40
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 15:12:43 +0000

    fix: 3 - DEREFERENCE_NULLABLE - int errorOverrideWidth = errorBuilder.getOverrideWidth(); - dereferenced expression errorBuilder is @Nullable

--- library/src/main/java/com/bumptech/glide/RequestBuilder.java
+++ library/src/main/java/com/bumptech/glide/RequestBuilder.java
@@ -38,6 +38,7 @@ import com.bumptech.glide.signature.AndroidResourceSignature;
 import com.bumptech.glide.util.Executors;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -1097,7 +1098,6 @@ public class RequestBuilder<TranscodeType> extends BaseRequestOptions<RequestBui
       BaseRequestOptions<?> requestOptions,
       Executor callbackExecutor) {
 
-    // Build the ErrorRequestCoordinator first if necessary so we can update parentCoordinator.
     ErrorRequestCoordinator errorRequestCoordinator = null;
     if (errorBuilder != null) {
       errorRequestCoordinator = new ErrorRequestCoordinator(requestLock, parentCoordinator);
@@ -1121,7 +1121,7 @@ public class RequestBuilder<TranscodeType> extends BaseRequestOptions<RequestBui
       return mainRequest;
     }
 
-    int errorOverrideWidth = errorBuilder.getOverrideWidth();
+    int errorOverrideWidth = Nullability.castToNonnull(errorBuilder.getOverrideWidth());
     int errorOverrideHeight = errorBuilder.getOverrideHeight();
     if (Util.isValidDimensions(overrideWidth, overrideHeight) && !errorBuilder.isValidOverride()) {
       errorOverrideWidth = requestOptions.getOverrideWidth();
