af4455348ead4e56eb75dd9275e54ee86bf31e53
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 15:16:10 +0000

    fix: 6 - METHOD_NO_INIT - @SuppressWarnings("PMD.ConstructorCallsOverridableMethod") - initializer method does not guarantee @NonNull field requestOptions (line 93) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- library/src/main/java/com/bumptech/glide/RequestBuilder.java
+++ library/src/main/java/com/bumptech/glide/RequestBuilder.java
@@ -38,6 +38,7 @@ import com.bumptech.glide.signature.AndroidResourceSignature;
 import com.bumptech.glide.util.Executors;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -101,7 +102,7 @@ public class RequestBuilder<TranscodeType> extends BaseRequestOptions<RequestBui
     this.glideContext = glide.getGlideContext();
 
     initRequestListeners(requestManager.getDefaultRequestListeners());
-    apply(requestManager.getDefaultRequestOptions());
+    apply(Nullability.castToNonnull(requestManager.getDefaultRequestOptions()));
   }
 
   RequestManager getRequestManager() {
