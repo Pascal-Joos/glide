5a403daeee9f85afe78cc1df261fff2a3f1b161f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:06:52 +0000

    Revert "fix: 34 - DEREFERENCE_NULLABLE - return path.load( - dereferenced expression path is @Nullable"
    This reverts commit 4b23fe0ee66a2acb1205829cce1af4aa40a38056.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -23,7 +23,6 @@ import com.bumptech.glide.util.pool.FactoryPools.Poolable;
 import com.bumptech.glide.util.pool.GlideTrace;
 import com.bumptech.glide.util.pool.StateVerifier;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -551,8 +550,8 @@ class DecodeJob<R>
     DataRewinder<Data> rewinder = glideContext.getRegistry().getRewinder(data);
     try {
       // ResourceType in DecodeCallback below is required for compilation to work with gradle.
-      return Nullability.castToNonnull(path)
-          .load(rewinder, options, width, height, new DecodeCallback<ResourceType>(dataSource));
+      return path.load(
+          rewinder, options, width, height, new DecodeCallback<ResourceType>(dataSource));
     } finally {
       rewinder.cleanup();
     }
