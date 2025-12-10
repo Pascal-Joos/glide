503288b57def7d5cce26c6ce279d8c36573068a2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:33:44 +0000

    Revert "fix: 64 - UNBOX_NULLABLE - int timeout = options.get(TIMEOUT); - unboxing of a @Nullable value - options.get(TIMEOUT)"
    This reverts commit 30f4150dfcfaaba208a31c52041c133168e58b5e.

--- library/src/main/java/com/bumptech/glide/load/model/stream/HttpGlideUrlLoader.java
+++ library/src/main/java/com/bumptech/glide/load/model/stream/HttpGlideUrlLoader.java
@@ -10,7 +10,6 @@ import com.bumptech.glide.load.model.ModelCache;
 import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoaderFactory;
 import com.bumptech.glide.load.model.MultiModelLoaderFactory;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.InputStream;
 
 /**
@@ -52,7 +51,7 @@ public class HttpGlideUrlLoader implements ModelLoader<GlideUrl, InputStream> {
         url = model;
       }
     }
-    int timeout = Nullability.castToNonnull(options.get(TIMEOUT));
+    int timeout = options.get(TIMEOUT);
     return new LoadData<>(url, new HttpUrlFetcher(url, timeout));
   }
 
