02f70e513e5e561c3d738febd2e7e2991e4830b5
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:31:57 +0000

    fix: 61 - UNBOX_NULLABLE - if (countForResult == 1) { - unboxing of a @Nullable value - countForResult

--- library/src/main/java/com/bumptech/glide/load/engine/prefill/PreFillQueue.java
+++ library/src/main/java/com/bumptech/glide/load/engine/prefill/PreFillQueue.java
@@ -1,5 +1,6 @@
 package com.bumptech.glide.load.engine.prefill;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -25,7 +26,7 @@ final class PreFillQueue {
     PreFillType result = keyList.get(keyIndex);
 
     Integer countForResult = bitmapsPerType.get(result);
-    if (countForResult == 1) {
+    if (Nullability.castToNonnull(countForResult) == 1) {
       bitmapsPerType.remove(result);
       keyList.remove(keyIndex);
     } else {
