943a14ac50beda327b981a421ba2e68fb7c36761
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:43:15 +0000

    fix: 15 - ASSIGN_FIELD_NULLABLE - model = null; - assigning @Nullable expression to @NonNull field

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -16,6 +16,7 @@ import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoader.LoadData;
 import com.bumptech.glide.load.resource.UnitTransformation;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -213,11 +214,13 @@ final class DecodeHelper<Transcode> {
     if (!isLoadDataSet) {
       isLoadDataSet = true;
       loadData.clear();
-      List<ModelLoader<Object, ?>> modelLoaders = glideContext.getRegistry().getModelLoaders(model);
+      List<ModelLoader<Object, ?>> modelLoaders =
+          glideContext.getRegistry().getModelLoaders(Nullability.castToNonnull(model));
       //noinspection ForLoopReplaceableByForEach to improve perf
       for (int i = 0, size = modelLoaders.size(); i < size; i++) {
         ModelLoader<Object, ?> modelLoader = modelLoaders.get(i);
-        LoadData<?> current = modelLoader.buildLoadData(model, width, height, options);
+        LoadData<?> current =
+            modelLoader.buildLoadData(Nullability.castToNonnull(model), width, height, options);
         if (current != null) {
           loadData.add(current);
         }
