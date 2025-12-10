fba27f9bd276a269ee133822d31bec41925163ea
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:37:11 +0000

    Revert "fix: 68 - RETURN_NULLABLE - return result; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit bb07132be66df272b80a66759d2dd918111a5963.

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
@@ -11,7 +11,6 @@ import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.util.Util;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
-import javax.annotation.Nullable;
 
 /**
  * A simple {@link com.bumptech.glide.load.Transformation} for transforming {@link
@@ -65,7 +64,6 @@ import javax.annotation.Nullable;
  */
 public abstract class BitmapTransformation implements Transformation<Bitmap> {
 
-  @Nullable
   @NonNull
   @Override
   public final Resource<Bitmap> transform(
