f8f3147e82ec0493173ed0b843e81356e82ec3b3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:22:44 +0000

    Revert "fix: 1 - RETURN_NULLABLE - return glide; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 3df746e298a3a0e8dd490de6090a2e012bb1249b.

--- library/src/main/java/com/bumptech/glide/load/resource/gif/ByteBufferGifDecoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/gif/ByteBufferGifDecoder.java
@@ -23,7 +23,6 @@ import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.UnitTransformation;
 import com.bumptech.glide.util.LogTime;
 import com.bumptech.glide.util.Util;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.List;
@@ -49,9 +48,9 @@ public class ByteBufferGifDecoder implements ResourceDecoder<ByteBuffer, GifDraw
   public ByteBufferGifDecoder(Context context) {
     this(
         context,
-        Nullability.castToNonnull(Glide.get(context).getRegistry().getImageHeaderParsers()),
-        Nullability.castToNonnull(Glide.get(context)),
-        Nullability.castToNonnull(Glide.get(context).getArrayPool()));
+        Glide.get(context).getRegistry().getImageHeaderParsers(),
+        Glide.get(context).getBitmapPool(),
+        Glide.get(context).getArrayPool());
   }
 
   public ByteBufferGifDecoder(
