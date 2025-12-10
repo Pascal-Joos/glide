6b1ebca2d70ce4d41ce99373e7f92b5402810ec8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:38:31 +0000

    Revert "fix: 69 - DEREFERENCE_NULLABLE - downsampleStrategy.getScaleFactor( - dereferenced expression downsampleStrategy is @Nullable"
    This reverts commit 101a374a2e227835c56b8ce3c121149c81e60816.

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
@@ -28,7 +28,6 @@ import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.util.LogTime;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -506,8 +505,8 @@ public final class Downsampler {
     }
 
     final float exactScaleFactor =
-        Nullability.castToNonnull(downsampleStrategy)
-            .getScaleFactor(orientedSourceWidth, orientedSourceHeight, targetWidth, targetHeight);
+        downsampleStrategy.getScaleFactor(
+            orientedSourceWidth, orientedSourceHeight, targetWidth, targetHeight);
 
     if (exactScaleFactor <= 0f) {
       throw new IllegalArgumentException(
