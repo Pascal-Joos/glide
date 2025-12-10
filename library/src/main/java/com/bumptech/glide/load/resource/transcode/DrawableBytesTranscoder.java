f8965cd836ce839bd8f9cb879f63b3ad4cd40281
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:46:32 +0000

    Revert "fix: 78 - PASS_NULLABLE - return bitmapBytesTranscoder.transcode( - passing @Nullable parameter 'BitmapResource.obtain(((BitmapDrawable) drawable).getBitmap(), bitmapPool)' where @NonNull is required"
    This reverts commit 209e10fb269ff15e33e093403ef15ac23d4c3969.

--- library/src/main/java/com/bumptech/glide/load/resource/transcode/DrawableBytesTranscoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/transcode/DrawableBytesTranscoder.java
@@ -10,7 +10,6 @@ import com.bumptech.glide.load.engine.Resource;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.bitmap.BitmapResource;
 import com.bumptech.glide.load.resource.gif.GifDrawable;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Obtains {@code byte[]} from {@link BitmapDrawable}s by delegating to a {@link ResourceTranscoder}
@@ -37,9 +36,7 @@ public final class DrawableBytesTranscoder implements ResourceTranscoder<Drawabl
     Drawable drawable = toTranscode.get();
     if (drawable instanceof BitmapDrawable) {
       return bitmapBytesTranscoder.transcode(
-          Nullability.castToNonnull(
-              BitmapResource.obtain(((BitmapDrawable) drawable).getBitmap(), bitmapPool)),
-          options);
+          BitmapResource.obtain(((BitmapDrawable) drawable).getBitmap(), bitmapPool), options);
     } else if (drawable instanceof GifDrawable) {
       return gifDrawableBytesTranscoder.transcode(toGifDrawableResource(toTranscode), options);
     }
