51bfd87b99069c651d9059a88e94d25bd29df853
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:27:23 +0000

    fix: 5 - DEREFERENCE_NULLABLE - return !options.isMemoryCacheable() && previous.isComplete(); - dereferenced expression previous is @Nullable

--- library/src/main/java/com/bumptech/glide/RequestBuilder.java
+++ library/src/main/java/com/bumptech/glide/RequestBuilder.java
@@ -38,6 +38,7 @@ import com.bumptech.glide.signature.AndroidResourceSignature;
 import com.bumptech.glide.util.Executors;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -868,7 +869,7 @@ public class RequestBuilder<TranscodeType> extends BaseRequestOptions<RequestBui
   // equivalent. See #2663 for additional context.
   private boolean isSkipMemoryCacheWithCompletePreviousRequest(
       BaseRequestOptions<?> options, @Nullable Request previous) {
-    return !options.isMemoryCacheable() && previous.isComplete();
+    return !options.isMemoryCacheable() && Nullability.castToNonnull(previous).isComplete();
   }
 
   /**
