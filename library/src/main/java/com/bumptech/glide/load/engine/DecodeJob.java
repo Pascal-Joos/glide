64466e8a458f4fc04ebdcf15dd551efe1f96052d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:56:51 +0000

    fix: 24 - DEREFERENCE_NULLABLE - return priority.ordinal(); - dereferenced expression priority is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -104,6 +104,9 @@ class DecodeJob<R>
       Options options,
       Callback<R> callback,
       int order) {
+    if (model == null) {
+      throw new NullPointerException("model must not be null");
+    }
     decodeHelper.init(
         glideContext,
         model,
@@ -208,6 +211,9 @@ class DecodeJob<R>
   }
 
   private int getPriority() {
+    if (priority == null) {
+      throw new NullPointerException("priority must not be null");
+    }
     return priority.ordinal();
   }
 
