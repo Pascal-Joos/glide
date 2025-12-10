1f53a0c6425e35d3f87eebac323f17d70bea4ab8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:36:06 +0000

    fix: 67 - UNBOX_NULLABLE - int quality = options.get(COMPRESSION_QUALITY); - unboxing of a @Nullable value - options.get(COMPRESSION_QUALITY)

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapEncoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapEncoder.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.load.engine.bitmap_recycle.ArrayPool;
 import com.bumptech.glide.util.LogTime;
 import com.bumptech.glide.util.Util;
 import com.bumptech.glide.util.pool.GlideTrace;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -73,7 +74,7 @@ public class BitmapEncoder implements ResourceEncoder<Bitmap> {
         "encode: [%dx%d] %s", bitmap.getWidth(), bitmap.getHeight(), format);
     try {
       long start = LogTime.getLogTime();
-      int quality = options.get(COMPRESSION_QUALITY);
+      int quality = Nullability.castToNonnull(options.get(COMPRESSION_QUALITY));
 
       boolean success = false;
       OutputStream os = null;
