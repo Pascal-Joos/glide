62af9819cd02aa27cb24ea9be899dccb82056e82
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:39:46 +0000

    Revert "fix: 71 - UNBOX_NULLABLE - boolean fixBitmapToRequestedDimensions = options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS); - unboxing of a @Nullable value - options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS)"
    This reverts commit 39f1a85793be8b1ef96c3eac416bbaa5ecc51228.

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
@@ -28,7 +28,6 @@ import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.util.LogTime;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -284,8 +283,7 @@ public final class Downsampler {
     DecodeFormat decodeFormat = options.get(DECODE_FORMAT);
     PreferredColorSpace preferredColorSpace = options.get(PREFERRED_COLOR_SPACE);
     DownsampleStrategy downsampleStrategy = options.get(DownsampleStrategy.OPTION);
-    boolean fixBitmapToRequestedDimensions =
-        Nullability.castToNonnull(options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS));
+    boolean fixBitmapToRequestedDimensions = options.get(FIX_BITMAP_SIZE_TO_REQUESTED_DIMENSIONS);
     boolean isHardwareConfigAllowed =
         options.get(ALLOW_HARDWARE_CONFIG) != null && options.get(ALLOW_HARDWARE_CONFIG);
 
