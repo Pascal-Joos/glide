c4d548055f8b000b0cd1e3169e03325a78367855
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:10:48 +0000

    fix: 49 - METHOD_NO_INIT - SourceGenerator(DecodeHelper<?> helper, FetcherReadyCallback cb) { - initializer method does not guarantee @NonNull field loadData (line 40) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- library/src/main/java/com/bumptech/glide/load/engine/SourceGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/SourceGenerator.java
@@ -43,6 +43,7 @@ class SourceGenerator implements DataFetcherGenerator, DataFetcherGenerator.Fetc
   SourceGenerator(DecodeHelper<?> helper, FetcherReadyCallback cb) {
     this.helper = helper;
     this.cb = cb;
+    this.loadData = null;
   }
 
   // Concurrent access isn't supported.
