ebe8d89219765d696c31caac88a2e888f637d41b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:32:25 +0000

    Revert "fix: 82 - DEREFERENCE_NULLABLE - this(new State(wrapped.getConstantState(), width, height), wrapped); - dereferenced expression wrapped is @Nullable"
    This reverts commit 6d837e2b92ca7fa93f8bf4b86cec2c47cb257058.

--- library/src/main/java/com/bumptech/glide/request/target/FixedSizeDrawable.java
+++ library/src/main/java/com/bumptech/glide/request/target/FixedSizeDrawable.java
@@ -32,9 +32,7 @@ public class FixedSizeDrawable extends Drawable {
   // Public API.
   @SuppressWarnings("WeakerAccess")
   public FixedSizeDrawable(@Nullable Drawable wrapped, int width, int height) {
-    this(
-        new State(Preconditions.checkNotNull(wrapped.getConstantState()), width, height),
-        Preconditions.checkNotNull(wrapped));
+    this(new State(wrapped.getConstantState(), width, height), wrapped);
   }
 
   FixedSizeDrawable(State state, Drawable wrapped) {
