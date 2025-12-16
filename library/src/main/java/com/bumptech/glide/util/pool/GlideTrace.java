27f0d99888748e30d0e8ec05e56d77947cb21cd8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:35:18 +0000

    Revert "fix: 87 - DEREFERENCE_NULLABLE - int cookie = COOKIE_CREATOR.incrementAndGet(); - dereferenced expression COOKIE_CREATOR is @Nullable"
    This reverts commit c6f4f768268a8b4bbdd2c101566be89d180ef905.

--- library/src/main/java/com/bumptech/glide/util/pool/GlideTrace.java
+++ library/src/main/java/com/bumptech/glide/util/pool/GlideTrace.java
@@ -51,7 +51,7 @@ public final class GlideTrace {
   }
 
   public static int beginSectionAsync(String tag) {
-    if (TRACING_ENABLED && COOKIE_CREATOR != null) {
+    if (TRACING_ENABLED) {
       int cookie = COOKIE_CREATOR.incrementAndGet();
       Trace.beginAsyncSection(truncateTag(tag), cookie);
       return cookie;
