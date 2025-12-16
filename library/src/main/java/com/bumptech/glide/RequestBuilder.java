438329fb83e4f74904fd07210ef6f4beacfd1aca
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:39:29 +0000

    fix: 4 - DEREFERENCE_NULLABLE - previous.begin(); - dereferenced expression previous is @Nullable

--- library/src/main/java/com/bumptech/glide/RequestBuilder.java
+++ library/src/main/java/com/bumptech/glide/RequestBuilder.java
@@ -839,7 +839,8 @@ public class RequestBuilder<TranscodeType> extends BaseRequestOptions<RequestBui
     Request request = buildRequest(target, targetListener, options, callbackExecutor);
 
     Request previous = target.getRequest();
-    if (request.isEquivalentTo(previous)
+    if (previous != null
+        && request.isEquivalentTo(previous)
         && !isSkipMemoryCacheWithCompletePreviousRequest(options, previous)) {
       // If the request is completed, beginning again will ensure the result is re-delivered,
       // triggering RequestListeners and Targets. If the request is failed, beginning again will
