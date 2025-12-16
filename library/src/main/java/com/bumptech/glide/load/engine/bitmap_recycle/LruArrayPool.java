704dc69470d3d17ce90f9a6351f075d13e562312
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:16:20 +0000

    fix: 57 - UNBOX_NULLABLE - currentSize += size * sortedSizes.get(type).get(size) * adapter.getElementSizeInBytes(); - unboxing of a @Nullable value - sortedSizes.get(type).get(size)

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/LruArrayPool.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/LruArrayPool.java
@@ -208,12 +208,21 @@ public final class LruArrayPool implements ArrayPool {
   }
 
   // VisibleForTesting
+  // VisibleForTesting
   int getCurrentSize() {
     int currentSize = 0;
     for (Class<?> type : sortedSizes.keySet()) {
-      for (Integer size : sortedSizes.get(type).keySet()) {
+      Map<Integer, Integer> sizesForType = sortedSizes.get(type);
+      if (sizesForType == null) {
+        continue;
+      }
+      for (Integer size : sizesForType.keySet()) {
+        Integer count = sizesForType.get(size);
+        if (count == null) {
+          continue;
+        }
         ArrayAdapterInterface<?> adapter = getAdapterFromType(type);
-        currentSize += size * sortedSizes.get(type).get(size) * adapter.getElementSizeInBytes();
+        currentSize += size * count * adapter.getElementSizeInBytes();
       }
     }
     return currentSize;
