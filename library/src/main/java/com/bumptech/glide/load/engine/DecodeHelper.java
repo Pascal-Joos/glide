f27c12423a4364669332218fc20ceb2ad63f7dd9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:48:27 +0000

    Revert "fix: 16 - ASSIGN_FIELD_NULLABLE - resourceClass = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 6bce4790eac624f99853c52c2b459aa549403adf.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -82,6 +82,7 @@ final class DecodeHelper<Transcode> {
     glideContext = null;
     model = null;
     signature = null;
+    resourceClass = null;
     transcodeClass = null;
     options = null;
     priority = null;
