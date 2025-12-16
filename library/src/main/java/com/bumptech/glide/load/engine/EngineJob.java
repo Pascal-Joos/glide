d15d3bd83fd574949469c6a53dc8e4153301e967
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:05:27 +0000

    fix: 41 - DEREFERENCE_NULLABLE - engineResource.acquire(); - dereferenced expression engineResource is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
@@ -430,7 +430,7 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
       // (b/136032534).
       synchronized (cb.getLock()) {
         synchronized (EngineJob.this) {
-          if (cbs.contains(cb)) {
+          if (cbs.contains(cb) && engineResource != null) {
             // Acquire for this particular callback.
             engineResource.acquire();
             callCallbackOnResourceReady(cb);
