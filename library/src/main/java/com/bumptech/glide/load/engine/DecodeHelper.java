c2385daba903bd043c85509b9058834d73edeed3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:51:22 +0000

    Revert "fix: 20 - RETURN_NULLABLE - return priority; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 5bd9591e778ce95426aa4c49bd7473114467cbcc.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -108,7 +108,7 @@ final class DecodeHelper<Transcode> {
   }
 
   Priority getPriority() {
-    return priority == null ? Priority.NORMAL : priority;
+    return priority;
   }
 
   Options getOptions() {
