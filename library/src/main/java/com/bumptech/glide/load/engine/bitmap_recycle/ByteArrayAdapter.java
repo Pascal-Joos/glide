fa71cb4fe39870384c84a7c1224e710868aa0bf3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:12:06 +0000

    Revert "fix: 51 - DEREFERENCE_NULLABLE - return array.length; - dereferenced expression array is @Nullable"
    This reverts commit c354b03a49645e5fad1767cf18ca0dfb622518f6.

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/ByteArrayAdapter.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/ByteArrayAdapter.java
@@ -14,9 +14,6 @@ public final class ByteArrayAdapter implements ArrayAdapterInterface<byte[]> {
 
   @Override
   public int getArrayLength(@Nullable byte[] array) {
-    if (array == null) {
-      return 0;
-    }
     return array.length;
   }
 
