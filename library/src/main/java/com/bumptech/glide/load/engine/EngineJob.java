fecdf636987cf038e56b5af0ad8f9121c1047d68
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:04:46 +0000

    fix: 40 - ASSIGN_FIELD_NULLABLE - decodeJob = null; - assigning @Nullable expression to @NonNull field

--- library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
@@ -312,7 +312,6 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
     hasResource = false;
     isLoadedFromAlternateCacheKey = false;
     decodeJob.release(/* isRemovedFromQueue= */ false);
-    decodeJob = null;
     exception = null;
     dataSource = null;
     pool.release(this);
