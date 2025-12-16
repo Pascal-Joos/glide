1faa792d40a087dce419f541a2a9b8d5c67f1580
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:28:08 +0000

    Revert "fix: 75 - UNBOX_NULLABLE - return !options.get(GifOptions.DISABLE_ANIMATION) - unboxing of a @Nullable value - options.get(GifOptions.DISABLE_ANIMATION)"
    This reverts commit b453a5cb8c7b6b515a0a58b55332b245ec284f6e.

--- library/src/main/java/com/bumptech/glide/load/resource/gif/ByteBufferGifDecoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/gif/ByteBufferGifDecoder.java
@@ -78,8 +78,7 @@ public class ByteBufferGifDecoder implements ResourceDecoder<ByteBuffer, GifDraw
 
   @Override
   public boolean handles(@NonNull ByteBuffer source, @NonNull Options options) throws IOException {
-    Boolean disableAnimation = options.get(GifOptions.DISABLE_ANIMATION);
-    return (disableAnimation == null || !disableAnimation)
+    return !options.get(GifOptions.DISABLE_ANIMATION)
         && ImageHeaderParserUtils.getType(parsers, source) == ImageType.GIF;
   }
 
