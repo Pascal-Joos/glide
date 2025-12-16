256daf6057d3abb613d880e8ff9ade985e9ccf04
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:01:06 +0000

    Revert "fix: 34 - DEREFERENCE_NULLABLE - return path.load( - dereferenced expression path is @Nullable"
    This reverts commit b6b9b744f00a18a3a183f5d4a97ef5d380a93c87.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -546,9 +546,6 @@ class DecodeJob<R>
   private <Data, ResourceType> Resource<R> runLoadPath(
       Data data, @Nullable DataSource dataSource, @Nullable LoadPath<Data, ResourceType, R> path)
       throws GlideException {
-    if (path == null) {
-      throw new GlideException("LoadPath must not be null");
-    }
     Options options = getOptionsWithHardwareConfig(dataSource);
     DataRewinder<Data> rewinder = glideContext.getRegistry().getRewinder(data);
     try {
