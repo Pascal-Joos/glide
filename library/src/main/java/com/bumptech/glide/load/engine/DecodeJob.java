54fb0014dc8a16373c27e646b7b13ff70bbe8ac6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:55:46 +0000

    fix: 26 - PASS_NULLABLE - decodeHelper.init( - passing @Nullable parameter 'model' where @NonNull is required

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -88,7 +88,7 @@ class DecodeJob<R>
   @Initializer
   DecodeJob<R> init(
       GlideContext glideContext,
-      @Nullable Object model,
+      @Nullable @NonNull Object model,
       EngineKey loadKey,
       Key signature,
       int width,
