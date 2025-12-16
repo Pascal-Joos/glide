a46340258bc74bba8eecab65f187666656219aea
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:56:32 +0000

    fix: 27 - WRONG_OVERRIDE_PARAM - @Override - parameter sourceKey is @NonNull, but parameter in superclass method com.bumptech.glide.load.engine.DataFetcherGenerator.FetcherReadyCallback.onDataFetcherReady(com.bumptech.glide.load.Key,java.lang.Object,com.bumptech.glide.load.data.DataFetcher<?>,com.bumptech.glide.load.DataSource,com.bumptech.glide.load.Key) is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -389,11 +389,11 @@ class DecodeJob<R>
 
   @Override
   public void onDataFetcherReady(
-      Key sourceKey,
-      @Nullable Object data,
-      DataFetcher<?> fetcher,
-      DataSource dataSource,
-      Key attemptedKey) {
+      @NonNull Key sourceKey,
+      @Nullable @NonNull Object data,
+      @NonNull DataFetcher<?> fetcher,
+      @NonNull DataSource dataSource,
+      @NonNull Key attemptedKey) {
     this.currentSourceKey = sourceKey;
     this.currentData = data;
     this.currentFetcher = fetcher;
