eb571bf16e67c9fb4aadf2f3d6aecf4c28d92243
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:23:31 +0000

    fix: 68 - RETURN_NULLABLE - return result; - returning @Nullable expression from method with @NonNull return type

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
@@ -86,7 +86,11 @@ public abstract class BitmapTransformation implements Transformation<Bitmap> {
     if (toTransform.equals(transformed)) {
       result = resource;
     } else {
-      result = BitmapResource.obtain(transformed, bitmapPool);
+      Resource<Bitmap> obtained = BitmapResource.obtain(transformed, bitmapPool);
+      if (obtained == null) {
+        return resource;
+      }
+      result = obtained;
     }
     return result;
   }
