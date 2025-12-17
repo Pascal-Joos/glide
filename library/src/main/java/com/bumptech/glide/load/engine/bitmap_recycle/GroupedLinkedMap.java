1a659750be38b07cdf39b6924931214b648ca232
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:30:32 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression values is @Nullable' /home/vscode/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/GroupedLinkedMap.java:142       return valueSize > 0 ? values.remove(valueSize - 1) : null;

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/GroupedLinkedMap.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/GroupedLinkedMap.java
@@ -142,7 +142,7 @@ class GroupedLinkedMap<K extends Poolable, V> {
     @Nullable
     public V removeLast() {
       final int valueSize = size();
-      return valueSize > 0 ? values.remove(valueSize - 1) : null;
+      return valueSize > 0 && values != null ? values.remove(valueSize - 1) : null;
     }
 
     public int size() {
