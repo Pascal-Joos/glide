773a63c20dda045eb1be4faefd6b543ed5db2f9d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:33:15 +0000

    fix: Type='UNBOX_NULLABLE', message='unboxing of a @Nullable value - options.get(TIMEOUT)' /home/vscode/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/model/stream/HttpGlideUrlLoader.java:54     int timeout = options.get(TIMEOUT);

--- library/src/main/java/com/bumptech/glide/load/model/stream/HttpGlideUrlLoader.java
+++ library/src/main/java/com/bumptech/glide/load/model/stream/HttpGlideUrlLoader.java
@@ -51,7 +51,8 @@ public class HttpGlideUrlLoader implements ModelLoader<GlideUrl, InputStream> {
         url = model;
       }
     }
-    int timeout = options.get(TIMEOUT);
+    Integer timeoutValue = options.get(TIMEOUT);
+    int timeout = timeoutValue != null ? timeoutValue : 0;
     return new LoadData<>(url, new HttpUrlFetcher(url, timeout));
   }
 
