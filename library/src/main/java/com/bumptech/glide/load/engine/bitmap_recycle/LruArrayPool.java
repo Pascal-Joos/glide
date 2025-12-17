4a7c7c0dad5c5dac10c288247af145cd4fa3730a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:31:22 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression object is @Nullable' /home/vscode/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/LruArrayPool.java:190     return (ArrayAdapterInterface<T>) getAdapterFromType(object.getClass());

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/LruArrayPool.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/LruArrayPool.java
@@ -189,6 +189,9 @@ public final class LruArrayPool implements ArrayPool {
 
   @SuppressWarnings("unchecked")
   private <T> ArrayAdapterInterface<T> getAdapterFromObject(@Nullable T object) {
+    if (object == null) {
+      throw new NullPointerException("object must not be null");
+    }
     return (ArrayAdapterInterface<T>) getAdapterFromType(object.getClass());
   }
 
