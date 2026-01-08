7a9e161a41d45bcf1b20a8395700e0d2032cb0a0
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 10:21:25 +0000

    fix: 38 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull fields key (line 47), decodeJob (line 73) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/EngineJob.java
@@ -15,6 +15,7 @@ import com.bumptech.glide.util.Synthetic;
 import com.bumptech.glide.util.pool.FactoryPools.Poolable;
 import com.bumptech.glide.util.pool.StateVerifier;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -44,7 +45,9 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
   private final GlideExecutor animationExecutor;
   private final AtomicInteger pendingCallbacks = new AtomicInteger();
 
+  @SuppressWarnings("NullAway.Init")
   private Key key;
+
   private boolean isCacheable;
   private boolean useUnlimitedSourceGeneratorPool;
   private boolean useAnimationPool;
@@ -70,7 +73,7 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
   @Synthetic
   EngineResource<?> engineResource;
 
-  private DecodeJob<R> decodeJob;
+  @Nullable private DecodeJob<R> decodeJob;
 
   // Checked primarily on the main thread, but also on other threads in reschedule.
   private volatile boolean isCancelled;
@@ -115,6 +118,7 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
     this.engineResourceFactory = engineResourceFactory;
   }
 
+  @Initializer
   @VisibleForTesting
   synchronized EngineJob<R> init(
       Key key,
@@ -210,7 +214,9 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
     }
 
     isCancelled = true;
-    decodeJob.cancel();
+    if (decodeJob != null) {
+      Nullability.castToNonnull(decodeJob).cancel();
+    }
     engineJobListener.onEngineJobCancelled(this, key);
   }
 
@@ -311,7 +317,9 @@ class EngineJob<R> implements DecodeJob.Callback<R>, Poolable {
     isCancelled = false;
     hasResource = false;
     isLoadedFromAlternateCacheKey = false;
-    decodeJob.release(/* isRemovedFromQueue= */ false);
+    if (decodeJob != null) {
+      decodeJob.release(/* isRemovedFromQueue= */ false);
+    }
     decodeJob = null;
     exception = null;
     dataSource = null;
