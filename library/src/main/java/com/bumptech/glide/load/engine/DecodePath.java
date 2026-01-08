86bf4f37c8db52bee4f918e14c447142ce0c6db5
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 10:19:29 +0000

    fix: 36 - DEREFERENCE_NULLABLE - List<Throwable> exceptions = Preconditions.checkNotNull(listPool.acquire()); - dereferenced expression listPool is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/DecodePath.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodePath.java
@@ -67,7 +67,7 @@ public class DecodePath<DataType, ResourceType, Transcode> {
   private Resource<ResourceType> decodeResource(
       DataRewinder<DataType> rewinder, int width, int height, @NonNull Options options)
       throws GlideException {
-    List<Throwable> exceptions = Preconditions.checkNotNull(listPool.acquire());
+    List<Throwable> exceptions = Preconditions.checkNotNull(listPool).acquire();
     try {
       return decodeResourceWithList(rewinder, width, height, options, exceptions);
     } finally {
