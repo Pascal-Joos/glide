946ebe0030cad99bc39119fc50cbadfd541d1e41
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:50:00 +0000

    fix: 4 - DEREFERENCE_NULLABLE - previous.begin(); - dereferenced expression previous is @Nullable

--- library/src/main/java/com/bumptech/glide/RequestBuilder.java
+++ library/src/main/java/com/bumptech/glide/RequestBuilder.java
@@ -38,6 +38,7 @@ import com.bumptech.glide.signature.AndroidResourceSignature;
 import com.bumptech.glide.util.Executors;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -849,7 +850,7 @@ public class RequestBuilder<TranscodeType> extends BaseRequestOptions<RequestBui
         // Use the previous request rather than the new one to allow for optimizations like skipping
         // setting placeholders, tracking and un-tracking Targets, and obtaining View dimensions
         // that are done in the individual Request.
-        previous.begin();
+        Nullability.castToNonnull(previous).begin();
       }
       return target;
     }
