f805a2df2dc71db94ffa7e061fa700ffbcf4ef27
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:48:22 +0000

    Revert "fix: 2 - PASS_NULLABLE - return Glide.get(context).getRequestManagerRetriever(); - passing @Nullable parameter 'context' where @NonNull is required"
    This reverts commit ee26b089d2fc62cc0595f82ec132b3d83dc56dda.

--- library/src/main/java/com/bumptech/glide/Glide.java
+++ library/src/main/java/com/bumptech/glide/Glide.java
@@ -36,7 +36,6 @@ import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.util.GlideSuppliers.GlideSupplier;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -517,7 +516,7 @@ public class Glide implements ComponentCallbacks2 {
     // Context could be null for other reasons (ie the user passes in null), but in practice it will
     // only occur due to errors with the Fragment lifecycle.
     Preconditions.checkNotNull(context, DESTROYED_ACTIVITY_WARNING);
-    return Glide.get(Nullability.castToNonnull(context)).getRequestManagerRetriever();
+    return Glide.get(context).getRequestManagerRetriever();
   }
 
   /**
