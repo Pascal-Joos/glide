ddce0b95bd8189c5e91d14c5b15652ef325195aa
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 10:42:21 +0000

    Revert "fix: 66 - DEREFERENCE_NULLABLE - strategy.getScaleFactor(size.getWidth(), size.getHeight(), targetWidth, targetHeight); - dereferenced expression strategy is @Nullable"
    This reverts commit 355a7c587c9cc5771f7148a636cc7b1763f6138b.

--- library/src/main/java/com/bumptech/glide/load/resource/DefaultOnHeaderDecodedListener.java
+++ library/src/main/java/com/bumptech/glide/load/resource/DefaultOnHeaderDecodedListener.java
@@ -49,8 +49,9 @@ public final class DefaultOnHeaderDecodedListener implements OnHeaderDecodedList
     this.requestedHeight = requestedHeight;
     decodeFormat = options.get(Downsampler.DECODE_FORMAT);
     strategy = options.get(DownsampleStrategy.OPTION);
-    Boolean allowHardwareConfig = options.get(Downsampler.ALLOW_HARDWARE_CONFIG);
-    isHardwareConfigAllowed = allowHardwareConfig != null && allowHardwareConfig;
+    isHardwareConfigAllowed =
+        options.get(Downsampler.ALLOW_HARDWARE_CONFIG) != null
+            && options.get(Downsampler.ALLOW_HARDWARE_CONFIG);
     preferredColorSpace = options.get(Downsampler.PREFERRED_COLOR_SPACE);
   }
 
@@ -90,10 +91,8 @@ public final class DefaultOnHeaderDecodedListener implements OnHeaderDecodedList
       targetHeight = size.getHeight();
     }
 
-    DownsampleStrategy nonNullStrategy = strategy == null ? DownsampleStrategy.DEFAULT : strategy;
     float scaleFactor =
-        nonNullStrategy.getScaleFactor(
-            size.getWidth(), size.getHeight(), targetWidth, targetHeight);
+        strategy.getScaleFactor(size.getWidth(), size.getHeight(), targetWidth, targetHeight);
 
     int resizeWidth = Math.round(scaleFactor * size.getWidth());
     int resizeHeight = Math.round(scaleFactor * size.getHeight());
