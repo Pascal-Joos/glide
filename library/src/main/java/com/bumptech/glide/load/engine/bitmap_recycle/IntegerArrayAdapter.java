907afd7aa3e0e3d96c3cb6ec30c966d34e04bac7
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 10:34:09 +0000

    Revert "fix: 54 - DEREFERENCE_NULLABLE - return array.length; - dereferenced expression array is @Nullable"
    This reverts commit 6939511c8fce03c6647dbed080488eb2cdd7d2d4.

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/IntegerArrayAdapter.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/IntegerArrayAdapter.java
@@ -1,6 +1,5 @@
 package com.bumptech.glide.load.engine.bitmap_recycle;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /** Adapter for handling primitive int arrays. */
@@ -15,7 +14,7 @@ public final class IntegerArrayAdapter implements ArrayAdapterInterface<int[]> {
 
   @Override
   public int getArrayLength(@Nullable int[] array) {
-    return Nullability.castToNonnull(array).length;
+    return array.length;
   }
 
   @Override
