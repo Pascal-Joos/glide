30d2711e3aad7317db78bcb5caafbb98376869d6
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 10:45:03 +0000

    Revert "fix: 69 - DEREFERENCE_NULLABLE - downsampleStrategy.getScaleFactor( - dereferenced expression downsampleStrategy is @Nullable"
    This reverts commit 1903fae8ba7701864d2af1aeb80c49ac29d4cc59.

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/Downsampler.java
@@ -28,7 +28,6 @@ import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.util.LogTime;
 import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Util;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -506,15 +505,15 @@ public final class Downsampler {
     }
 
     final float exactScaleFactor =
-        Nullability.castToNonnull(downsampleStrategy)
-            .getScaleFactor(orientedSourceWidth, orientedSourceHeight, targetWidth, targetHeight);
+        downsampleStrategy.getScaleFactor(
+            orientedSourceWidth, orientedSourceHeight, targetWidth, targetHeight);
 
     if (exactScaleFactor <= 0f) {
       throw new IllegalArgumentException(
           "Cannot scale with factor: "
               + exactScaleFactor
               + " from: "
-              + Nullability.castToNonnull(downsampleStrategy)
+              + downsampleStrategy
               + ", source: ["
               + sourceWidth
               + "x"
@@ -528,9 +527,8 @@ public final class Downsampler {
     }
 
     SampleSizeRounding rounding =
-        Nullability.castToNonnull(downsampleStrategy)
-            .getSampleSizeRounding(
-                orientedSourceWidth, orientedSourceHeight, targetWidth, targetHeight);
+        downsampleStrategy.getSampleSizeRounding(
+            orientedSourceWidth, orientedSourceHeight, targetWidth, targetHeight);
     if (rounding == null) {
       throw new IllegalArgumentException("Cannot round with null rounding");
     }
@@ -610,8 +608,8 @@ public final class Downsampler {
     }
 
     double adjustedScaleFactor =
-        Nullability.castToNonnull(downsampleStrategy)
-            .getScaleFactor(powerOfTwoWidth, powerOfTwoHeight, targetWidth, targetHeight);
+        downsampleStrategy.getScaleFactor(
+            powerOfTwoWidth, powerOfTwoHeight, targetWidth, targetHeight);
 
     // Density scaling is only supported if inBitmap is null prior to KitKat. Avoid setting
     // densities here so we calculate the final Bitmap size correctly.
