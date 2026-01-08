d8e31c6fa860df075ce8568aea966755b89f0ea7
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:57:14 +0000

    fix: 11 - DEREFERENCE_NULLABLE - cb.onDataFetcherFailed(sourceKey, e, loadData.fetcher, DataSource.DATA_DISK_CACHE); - dereferenced expression loadData is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
@@ -71,10 +71,14 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
       loadData = null;
       boolean started = false;
       while (!started && hasNextModelLoader()) {
+        File localCacheFile = cacheFile;
+        if (localCacheFile == null) {
+          return false;
+        }
         ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
         loadData =
             modelLoader.buildLoadData(
-                cacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
+                localCacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
         if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
           started = true;
           loadData.fetcher.loadData(helper.getPriority(), this);
@@ -100,11 +104,17 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
 
   @Override
   public void onDataReady(@Nullable Object data) {
-    cb.onDataFetcherReady(sourceKey, data, loadData.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
+    LoadData<?> local = loadData;
+    if (local != null) {
+      cb.onDataFetcherReady(sourceKey, data, local.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
+    }
   }
 
   @Override
   public void onLoadFailed(@NonNull Exception e) {
-    cb.onDataFetcherFailed(sourceKey, e, loadData.fetcher, DataSource.DATA_DISK_CACHE);
+    LoadData<?> local = loadData;
+    if (local != null) {
+      cb.onDataFetcherFailed(sourceKey, e, local.fetcher, DataSource.DATA_DISK_CACHE);
+    }
   }
 }
