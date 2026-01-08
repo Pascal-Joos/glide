2afd65946cfdeb20655110d4580d2e319f56b3e4
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 10:33:21 +0000

    fix: 53 - DEREFERENCE_NULLABLE - return valueSize > 0 ? values.remove(valueSize - 1) : null; - dereferenced expression values is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/GroupedLinkedMap.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/GroupedLinkedMap.java
@@ -139,7 +139,10 @@ class GroupedLinkedMap<K extends Poolable, V> {
     @Nullable
     public V removeLast() {
       final int valueSize = size();
-      return valueSize > 0 ? values.remove(valueSize - 1) : null;
+      if (values == null || valueSize <= 0) {
+        return null;
+      }
+      return values.remove(valueSize - 1);
     }
 
     public int size() {
